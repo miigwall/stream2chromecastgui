6a4734834363bd0722364e15608331aea5a0702b
Miika <info@ajatuksella.com>
2017-09-04 22:22:24 +0300

    changes thread to process

--- cc.py
+++ cc.py
@@ -1,5 +1,8 @@
 import kivy
+
 kivy.require('1.0.6') # replace with your current kivy version !
+
+import platform, os, stream2chromecast, atexit, sys, os.path
 
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
@@ -14,12 +17,10 @@ from kivy.clock import Clock
 from kivy.uix.filechooser import FileChooserListView
 from os.path import sep, expanduser, isdir, dirname
 from functools import partial
-import platform, os, stream2chromecast
 from threading import Thread
+from multiprocessing import Process
 from collections import namedtuple
 from kivy.config import Config
-import atexit
-import sys
 
 Config.set('kivy', 'exit_on_escape', '0')
 
@@ -29,6 +30,8 @@ csystem = platform.system()
 
 def exit_handler():
 	print ('EXIT')
+
+atexit.register(exit_handler)
 
 #
 # Start Cast (button)
@@ -48,10 +51,10 @@ class StartCastButton(Widget):
 
 	def stop_cast_on_device(self, device, instance):
 		stream2chromecast.stop(device[0])
-		sys.exit()
-		#self.cthread.end()
-		#self.casting_popup.dismiss()
-		#self.waiting_devices_popup.dismiss()
+		#sys.exit()
+		self.cthread.terminate()
+		self.casting_popup.dismiss()
+		self.waiting_devices_popup.dismiss()
 
 	def unpause_cast_on_device(self, device, instance):
 		stream2chromecast.unpause(device[0])
@@ -66,7 +69,7 @@ class StartCastButton(Widget):
 		stream2chromecast.volume_down(device[0])
 
 	def play_cast_on_device(self, chosenFile, device, instance):
-		self.cthread = cthread = Thread(target=self.play_cast, args=(chosenFile, device,))
+		self.cthread = cthread = Process(target=self.play_cast, args=(chosenFile, device,))
 		self.cthread.start()
 
 	def start_cast_on_device(self, chosenFile, device, instance):
@@ -76,15 +79,16 @@ class StartCastButton(Widget):
 		
 		self.casting_popup_content = GridLayout(cols=1)
 		self.casting_popup_content.add_widget(Label(text='Casting...'))
+		self.casting_popup_content.add_widget(Label(text=chosenFile, text_size=(400, None), halign='center'))
 
 		# Volume buttons
 		self.casting_popup_volume_buttons = GridLayout(cols=2)
 
-		self.casting_popup_volumedown_button = Button(text = 'Volume down')
+		self.casting_popup_volumedown_button = Button(text = 'Volume down', background_color=[2,1,2,1])
 		self.casting_popup_volumedown_button.bind(on_press=partial(self.volume_down_on_device, device))
 		self.casting_popup_volume_buttons.add_widget(self.casting_popup_volumedown_button)
 
-		self.casting_popup_volumeup_button = Button(text = 'Volume up')
+		self.casting_popup_volumeup_button = Button(text = 'Volume up', background_color=[2,1,2,1])
 		self.casting_popup_volumeup_button.bind(on_press=partial(self.volume_up_on_device, device))
 		self.casting_popup_volume_buttons.add_widget(self.casting_popup_volumeup_button)
 
@@ -93,15 +97,15 @@ class StartCastButton(Widget):
 		# Playback control buttons
 		self.casting_popup_playback_buttons = GridLayout(cols=3)
 		
-		self.casting_popup_pause_button = Button(text = 'Pause')
+		self.casting_popup_pause_button = Button(text = 'Pause', background_color=[0,1,2,1])
 		self.casting_popup_pause_button.bind(on_press=partial(self.pause_cast_on_device, device))
 		self.casting_popup_playback_buttons.add_widget(self.casting_popup_pause_button)
 		
-		self.casting_popup_continue_button = Button(text = 'Continue')
+		self.casting_popup_continue_button = Button(text = 'Continue', background_color=[0,1,2,1])
 		self.casting_popup_continue_button.bind(on_press=partial(self.unpause_cast_on_device, device))
 		self.casting_popup_playback_buttons.add_widget(self.casting_popup_continue_button)
 
-		self.casting_popup_stop_button = Button(text = 'Stop')
+		self.casting_popup_stop_button = Button(text = 'Stop', background_color=[3,0,2,1])
 		self.casting_popup_stop_button.bind(on_press=partial(self.stop_cast_on_device, device))
 		self.casting_popup_playback_buttons.add_widget(self.casting_popup_stop_button)
 
@@ -119,11 +123,11 @@ class StartCastButton(Widget):
 		popup_content = GridLayout(cols=1)
 
 		for device in devices:
-			cast_device_button = Button(text = device[1])
+			cast_device_button = Button(text = device[1] + ' (' + device[0] + ')', background_color=[0,1,2,1])
 			cast_device_button.bind(on_press = partial(self.start_cast_on_device, chosenFile, device))
 			popup_content.add_widget(cast_device_button)
 
-		cancel_button = Button(text = 'Cancel')
+		cancel_button = Button(text = 'Cancel', background_color=[3,0,2,1])
 		cancel_button.bind(on_press = self.cancel_cast)
 
 		popup_content.add_widget(cancel_button)
@@ -138,9 +142,7 @@ class StartCastButton(Widget):
 			self.waiting_devices_popup.bind(on_open = partial(self.list_devices, chosenFile))
 			self.waiting_devices_popup.content = Label(text='Searching for devices, please wait...')
 			self.waiting_devices_popup.open()
-			
-			print ('cast... cast... cast...')
-
+		
 		return True
 
 #
@@ -154,12 +156,15 @@ class FileScreen(GridLayout):
 
 		self.cols = 1
 		self.username = TextInput(multiline = False)
-		
-		self.file_button = Button(text = 'Choose a file...')
+
+		self.logo_text = Label(text='Stream2chromecast GUI', font_size = '30sp')
+		self.add_widget(self.logo_text)
+
+		self.file_button = Button(text = 'Choose a file...', background_color=[0,1,2,1], text_size=(400, None), halign='center')
 		self.file_button.bind(on_press=self.open_file_chooser)
 		self.add_widget(self.file_button)
 
-		self.cast_button = Button(text = 'Cannot cast yet. Choose a file first!')
+		self.cast_button = Button(text = 'Cannot cast yet. Choose a file first!', background_color=[3,0,2,1])
 		self.add_widget(self.cast_button)
 
 		self.chosenFile = ''
@@ -173,7 +178,7 @@ class FileScreen(GridLayout):
 		self.fileChooserPopupContent = GridLayout(cols=1)
 		self.fileChooserPopupContent.add_widget(self.fileChooser)
 
-		self.fileChooserChooseBtn = Button(text = 'Choose')
+		self.fileChooserChooseBtn = Button(text = 'Choose', background_color = [0,2,0,1])
 		self.fileChooserChooseBtn.bind(on_press = self.choose_file)
 
 		self.fileChooserPopupContent.add_widget(self.fileChooserChooseBtn)
@@ -182,10 +187,11 @@ class FileScreen(GridLayout):
 	def choose_file(self, instance):
 		cb = StartCastButton()
 
-		if len(self.fileChooser.selection) > 0:
+		if len(self.fileChooser.selection) > 0 and os.path.isfile(self.fileChooser.selection[0]):
 			self.chosenFile = self.fileChooser.selection[0]
 			self.file_button.text = self.chosenFile
 			self.cast_button.text = 'Cast now'
+			self.cast_button.background_color = [0,2,0,1]
 			self.cast_button.bind(on_press = partial(cb.on_cast_start, self.chosenFile))
 
 		print (self.chosenFile)
@@ -199,10 +205,10 @@ class FileScreen(GridLayout):
 # App
 #
 class MyApp(App):
-		def build(self):
-			return FileScreen()
+	title = 'Stream2chromecast GUI'
+
+	def build(self):
+		return FileScreen()
 
 if __name__ == '__main__':
-		MyApp().run()
-
-atexit.register(exit_handler)
+	MyApp().run()
